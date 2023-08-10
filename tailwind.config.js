ac7a1e89620638f794a712e90e37c55dfdfbdec6
chituenguyen <tuenguyen553@gmail.com>
2023-08-10 10:50:45 +0700

    add new font Uniscore

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        beVietNam: ["Be Vietnam Pro", "sans-serif"],
+        Uniscore: ["UniscoreSans"],
       },
       colors: {
         "basic": "#555",
